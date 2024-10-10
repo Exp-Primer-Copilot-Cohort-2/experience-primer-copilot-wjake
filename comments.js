6b6a9e1757a0300ba5c10f4c5cfb7a34f0f6cb84
Jake <50133276+wjake@users.noreply.github.com>
2024-10-10 12:53:04 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+// Run server
+// Server listens to request
+// If request is a POST request
+// Parse the POST data
+// Save the data to the comments file
+// If request is a GET request
+// Read the comments file
+// Send the contents of the file back to the client
